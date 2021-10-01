08f1d8043312b47efd8affdac3c50f928d2514f9
Renan <renanfprecechan@gmail.com>
2021-10-01 17:54:56 -0300

    Create script.js

---
+++ site/script.js
@@ -0,0 +1,31 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    msg.innerHTML =` Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#ff7f50'
+        foto.style.background = '#ff7f50bd'
+        msg.innerHTML = `Bom Dia <br> Agora são ${hora} horas e ${min} minutos.`
+         
+    } 
+    else if (hora >=12 && hora < 18) {
+        //boa tarde
+        img.src = 'fototarde.png'
+        msg.innerHTML = `Boa Tarde <br> Agora são ${hora} horas e ${min} minutos.`
+       document.body.style.background = '#912700bd'
+       
+    }
+     else {
+            //boa noite
+            img.src = 'fotonoite.png'
+            document.body.style.background = '#002140'
+            msg.innerHTML = `Boa noite <br> Agora são ${hora} horas e ${min} minutos.`
+            
+    
+        }
+    }
